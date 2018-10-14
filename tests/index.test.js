9467c54f1541053e92ca15d9254cbcd8858b7020
Aaron Wells <aaronw@catalyst.net.nz>
2018-10-14 16:16:00 +1300

    Tweaking test scripts

--- tests/index.test.js
+++ tests/index.test.js
@@ -10,13 +10,16 @@ const originalText = fs
 const filters = ["chef", "LOLCAT", "cockney"].map(f => [f]);
 
 test.each(filters)("Comparing filter output to Unix version", filterName => {
-  const expectedTransform = fs.readFileSync(
-    path.join(__dirname, `moby-dick-chapter-1.${filterName}.txt`),
-    { encoding: "UTF8" }
-  );
+  const expectedTransform = fs
+    .readFileSync(
+      path.join(__dirname, `moby-dick-chapter-1.${filterName}.txt`),
+      { encoding: "UTF8" }
+    )
+    .toString();
 
   const filterFn = require(`../src/${filterName}`).default;
-  const generatedTransform = filterFn(originalText);
 
-  expect(generatedTransform).toStrictEqual(expectedTransform);
+  expect(filterFn(originalText).split(" ")).toEqual(
+    expectedTransform.split(" ")
+  );
 });
