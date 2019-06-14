b7a1fc35121ced62ae8804930d3c753b37a24627
Aaron Wells <aaronw@catalyst.net.nz>
2019-06-14 12:50:56 +1200

    Switch to testing against snapshots
    There are still some differences in output v the originals,
    when dealing with input that includes line breaks. But I
    think it's close enough now to not let that block me from
    moving forward.

--- tests/against-originals.test.js
+++ tests/against-originals.test.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * Test if the output of the JS filters is identical to the output of the
+ * original programs.
+ */
 const sampleTexts = ['moby-dick-chapter-1.txt', 'manpage.txt'];
 const filters = [
   'b1ff',
@@ -12,14 +16,14 @@ const filters = [
   'jethro',
   'ken',
   'kenny',
-  'ky00te',
+  'ky00te', // currently failing
   'kraut',
   'LOLCAT',
   'nethackify',
   'newspeak',
   'nyc',
   'pirate',
-  'rasterman',
+  'rasterman', // currently failing
   'scottish',
   'scramble',
   'spammer',
@@ -33,9 +37,15 @@ describe.each(filters)('%s', (filterName) => {
     const originalText = fs.readFileSync(path.join(__dirname, filename), {
       encoding: 'UTF8',
     });
+    const expectedTransform = fs.readFileSync(
+      path.join(__dirname, `${filename}.${filterName}.txt`),
+      {
+        encoding: 'UTF8',
+      }
+    );
     const filterFn = require(`../src/${filterName}`);
     const transformedText = filterFn(originalText);
 
-    expect(transformedText.split(/\s/)).toMatchSnapshot();
+    expect(transformedText.split(/\s/)).toEqual(expectedTransform.split(/\s/));
   });
 });
