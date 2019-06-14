f587bc96fca8a16e51010c8f89b41735a717351b
Aaron Wells <aaronw@catalyst.net.nz>
2019-06-14 19:23:04 +1200

    Updates

---
+++ precache-manifest.e1caeab657dca466030e2184dbf72f74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8702084893ab410a435460c925a46f3",
+    "url": "/talk-like-a/index.html"
+  },
+  {
+    "revision": "5573c5aecb9131cd3a88",
+    "url": "/talk-like-a/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "36b3a47872e26ac28940",
+    "url": "/talk-like-a/static/js/2.99071623.chunk.js"
+  },
+  {
+    "revision": "5573c5aecb9131cd3a88",
+    "url": "/talk-like-a/static/js/main.5d52f27b.chunk.js"
+  },
+  {
+    "revision": "6076b8f456d86de36f47",
+    "url": "/talk-like-a/static/js/runtime~main.41073ffa.js"
+  }
+]);
