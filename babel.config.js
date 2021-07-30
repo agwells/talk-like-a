ab0f3af362b77ced9e6d6da8f677cdae2002c422
Aaron Wells <aaronw@catalyst.net.nz>
2021-07-30 15:16:34 +1200

    Convert tests to TypeScript

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+// This is only needed for Jest, because it uses
+// babel to process TypeScript files.
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+};
